5c51ea1125a306ca723d9a51c8d004124e836332
Bryan <bryan.adams.savard1@gmail.com>
2026-02-03 08:54:30 -0500

    js fini

---
+++ site/js/script.js
@@ -0,0 +1,11 @@
+const btPlus = document.querySelector('.js-plus');
+const conteneur = document.querySelector('.conteneur-transport');
+
+btPlus.addEventListener('click', fouvreferme);
+
+
+function fouvreferme(){
+
+conteneur.classList.toggle('conteneur-transport-invisible')
+
+};
